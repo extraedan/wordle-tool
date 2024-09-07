b4c47e272bf7672fe702c22b9eb2bfca0a3cc7a3
Edan <edanpchen@gmail.com>
2024-09-07 10:43:26 -0400

    Set up constants

---
+++ main.py
@@ -0,0 +1,9 @@
+# Constants
+CURRENT_GUESS_LENGTH = 5
+FIXED_PART = ["_", "_", "I", "L", "_"]
+REMAINING_LETTERS = ["Q","I","L","H","J","Z","X","C","B","M"]
+KNOWN_LETTERS = ["H", "C"]
+
+# Find which slots are blank
+temp_word = FIXED_PART
+temp_lets = KNOWN_LETTERS

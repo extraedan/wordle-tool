e69a34b6d03bb08816531e5aaa616a520dee562c
Edan <edanpchen@gmail.com>
2024-09-07 10:44:52 -0400

    Create function to generate possible combinations of letters

--- main.py
+++ main.py
@@ -7,3 +7,25 @@ KNOWN_LETTERS = ["H", "C"]
 # Find which slots are blank
 temp_word = FIXED_PART
 temp_lets = KNOWN_LETTERS
+
+def get_blanks(word):
+    return [index for index, value in enumerate(word) if value == "_"]
+
+def generate_combos(blank_list, temp_lets):
+    for start_index in range(len(temp_lets)):
+        rotated_temp_lets = temp_lets[start_index:] + temp_lets[:start_index]
+
+        for blank, letter in zip(blank_list, rotated_temp_lets):
+            temp_word[blank] = letter
+
+        # Find the last unused blank (if any)
+        used_blanks = set(blank_list[:len(rotated_temp_lets)])
+        remaining_blanks = [blank for blank in blank_list if blank not in used_blanks]
+
+        # Now fill the last unused blank with each letter from REMAINING_LETTERS
+        if remaining_blanks:
+            last_blank = remaining_blanks[0]  # Assuming only one last blank to fill
+            for letter in REMAINING_LETTERS:
+                temp_word[last_blank] = letter
+                printable = ''.join(temp_word).lower()
+                print(printable)
